ae49971e6839c15e37790f8ea972c243cbd694a9
Ibrahim Khatri <104985194+IbrahimKhatrii@users.noreply.github.com>
2025-07-14 22:37:06 +0530

    Update views.js

--- views.js
+++ views.js
@@ -1,5 +1,5 @@
 // views.js
 
-import { Column } from './views/column.js';
+import { LayrColumn } from './views/column.js';
 // In the future you can import more components here
 // import { LayrButton } from './views/button.js';
